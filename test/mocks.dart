b048b461fa036a21b7d0c5c33cea2f9c5ea6344b
Andrea Bizzotto <bizz84@gmail.com>
2020-01-25 11:52:11 +0000

    Add service builders at the top of the widget tree. (#2)
    Mocking Database in widget tests

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
+import 'package:starter_architecture_flutter_firebase/services/database.dart';
 import 'package:starter_architecture_flutter_firebase/services/firebase_auth_service.dart';
 
 class MockAuthService extends Mock implements FirebaseAuthService {}
+
+class MockDatabase extends Mock implements FirestoreDatabase {}
 
 class MockWidgetsBinding extends Mock implements WidgetsBinding {}
 
