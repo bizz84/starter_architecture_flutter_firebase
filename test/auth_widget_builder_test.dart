94864c09851d61f557c20403b22ac581d7ad52dc
bizz84 <bizz84@gmail.com>
2020-01-29 13:44:05 +0000

    Add group in test file

--- test/auth_widget_builder_test.dart
+++ test/auth_widget_builder_test.dart
@@ -10,97 +10,99 @@ import 'package:starter_architecture_flutter_firebase/services/firebase_auth_ser
 import 'mocks.dart';
 
 void main() {
-  MockAuthService mockAuthService;
-  MockDatabase mockDatabase;
-  StreamController<User> onAuthStateChangedController;
+  group('AuthWidgetBuilder tests', () {
+    MockAuthService mockAuthService;
+    MockDatabase mockDatabase;
+    StreamController<User> onAuthStateChangedController;
 
-  setUp(() {
-    mockAuthService = MockAuthService();
-    mockDatabase = MockDatabase();
-    onAuthStateChangedController = StreamController<User>();
-  });
-
-  tearDown(() {
-    mockAuthService = null;
-    mockDatabase = null;
-    onAuthStateChangedController.close();
-  });
-
-  void stubOnAuthStateChangedYields(Iterable<User> onAuthStateChanged) {
-    onAuthStateChangedController
-        .addStream(Stream<User>.fromIterable(onAuthStateChanged));
-    when(mockAuthService.onAuthStateChanged).thenAnswer((_) {
-      return onAuthStateChangedController.stream;
+    setUp(() {
+      mockAuthService = MockAuthService();
+      mockDatabase = MockDatabase();
+      onAuthStateChangedController = StreamController<User>();
     });
-  }
 
-  Future<void> pumpAuthWidget(
-      WidgetTester tester,
-      {@required
-          Widget Function(BuildContext, AsyncSnapshot<User>) builder}) async {
-    await tester.pumpWidget(
-      Provider<FirebaseAuthService>(
-        create: (_) => mockAuthService,
-        child: AuthWidgetBuilder(
-          databaseBuilder: (_, uid) => mockDatabase,
-          builder: builder,
+    tearDown(() {
+      mockAuthService = null;
+      mockDatabase = null;
+      onAuthStateChangedController.close();
+    });
+
+    void stubOnAuthStateChangedYields(Iterable<User> onAuthStateChanged) {
+      onAuthStateChangedController
+          .addStream(Stream<User>.fromIterable(onAuthStateChanged));
+      when(mockAuthService.onAuthStateChanged).thenAnswer((_) {
+        return onAuthStateChangedController.stream;
+      });
+    }
+
+    Future<void> pumpAuthWidget(
+        WidgetTester tester,
+        {@required
+            Widget Function(BuildContext, AsyncSnapshot<User>) builder}) async {
+      await tester.pumpWidget(
+        Provider<FirebaseAuthService>(
+          create: (_) => mockAuthService,
+          child: AuthWidgetBuilder(
+            databaseBuilder: (_, uid) => mockDatabase,
+            builder: builder,
+          ),
         ),
-      ),
-    );
-    await tester.pump(Duration.zero);
-  }
+      );
+      await tester.pump(Duration.zero);
+    }
 
-  testWidgets(
-      'WHEN onAuthStateChanged in waiting state'
-      'THEN calls builder with snapshot in waiting state'
-      'AND doesn\'t find MultiProvider', (WidgetTester tester) async {
-    stubOnAuthStateChangedYields(<User>[]);
+    testWidgets(
+        'WHEN onAuthStateChanged in waiting state'
+        'THEN calls builder with snapshot in waiting state'
+        'AND doesn\'t find MultiProvider', (WidgetTester tester) async {
+      stubOnAuthStateChangedYields(<User>[]);
 
-    final snapshots = <AsyncSnapshot<User>>[];
-    await pumpAuthWidget(tester, builder: (context, userSnapshot) {
-      snapshots.add(userSnapshot);
-      return Container();
+      final snapshots = <AsyncSnapshot<User>>[];
+      await pumpAuthWidget(tester, builder: (context, userSnapshot) {
+        snapshots.add(userSnapshot);
+        return Container();
+      });
+      expect(snapshots, [
+        AsyncSnapshot<User>.withData(ConnectionState.waiting, null),
+      ]);
+      expect(find.byType(MultiProvider), findsNothing);
     });
-    expect(snapshots, [
-      AsyncSnapshot<User>.withData(ConnectionState.waiting, null),
-    ]);
-    expect(find.byType(MultiProvider), findsNothing);
-  });
 
-  testWidgets(
-      'WHEN onAuthStateChanged returns null user'
-      'THEN calls builder with null user and active state'
-      'AND doesn\'t find MultiProvider', (WidgetTester tester) async {
-    stubOnAuthStateChangedYields(<User>[null]);
+    testWidgets(
+        'WHEN onAuthStateChanged returns null user'
+        'THEN calls builder with null user and active state'
+        'AND doesn\'t find MultiProvider', (WidgetTester tester) async {
+      stubOnAuthStateChangedYields(<User>[null]);
 
-    final snapshots = <AsyncSnapshot<User>>[];
-    await pumpAuthWidget(tester, builder: (context, userSnapshot) {
-      snapshots.add(userSnapshot);
-      return Container();
+      final snapshots = <AsyncSnapshot<User>>[];
+      await pumpAuthWidget(tester, builder: (context, userSnapshot) {
+        snapshots.add(userSnapshot);
+        return Container();
+      });
+      expect(snapshots, [
+        AsyncSnapshot<User>.withData(ConnectionState.waiting, null),
+        AsyncSnapshot<User>.withData(ConnectionState.active, null),
+      ]);
+      expect(find.byType(MultiProvider), findsNothing);
     });
-    expect(snapshots, [
-      AsyncSnapshot<User>.withData(ConnectionState.waiting, null),
-      AsyncSnapshot<User>.withData(ConnectionState.active, null),
-    ]);
-    expect(find.byType(MultiProvider), findsNothing);
-  });
 
-  testWidgets(
-      'WHEN onAuthStateChanged returns valid user'
-      'THEN calls builder with same user and active state'
-      'AND finds MultiProvider', (WidgetTester tester) async {
-    final user = User(uid: '123');
-    stubOnAuthStateChangedYields(<User>[user]);
+    testWidgets(
+        'WHEN onAuthStateChanged returns valid user'
+        'THEN calls builder with same user and active state'
+        'AND finds MultiProvider', (WidgetTester tester) async {
+      final user = User(uid: '123');
+      stubOnAuthStateChangedYields(<User>[user]);
 
-    final snapshots = <AsyncSnapshot<User>>[];
-    await pumpAuthWidget(tester, builder: (context, userSnapshot) {
-      snapshots.add(userSnapshot);
-      return Container();
+      final snapshots = <AsyncSnapshot<User>>[];
+      await pumpAuthWidget(tester, builder: (context, userSnapshot) {
+        snapshots.add(userSnapshot);
+        return Container();
+      });
+      expect(snapshots, [
+        AsyncSnapshot<User>.withData(ConnectionState.waiting, null),
+        AsyncSnapshot<User>.withData(ConnectionState.active, user),
+      ]);
+      expect(find.byType(MultiProvider), findsOneWidget);
     });
-    expect(snapshots, [
-      AsyncSnapshot<User>.withData(ConnectionState.waiting, null),
-      AsyncSnapshot<User>.withData(ConnectionState.active, user),
-    ]);
-    expect(find.byType(MultiProvider), findsOneWidget);
   });
 }
