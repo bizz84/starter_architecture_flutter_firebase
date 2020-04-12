7ee8912f0a7cb1360fadee923b8060e75e7d21bf
bizz84 <bizz84@gmail.com>
2020-04-12 22:26:58 +0100

    Enable web project (flutter create .)

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,20 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  FirestoreWeb.registerWith(registry.registrarFor(FirestoreWeb));
+  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  registry.registerMessageHandler();
+}
