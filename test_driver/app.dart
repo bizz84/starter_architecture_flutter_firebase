a057f33f287b4c97704d4199a3b24740a6617a21
Andrea Bizzotto <bizz84@gmail.com>
2020-09-18 14:26:05 +0100

    Call Firebase.initializeApp() in driver test to avoid runtime error

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,4 +1,5 @@
 //import 'package:starter_architecture_flutter_firebase/main.dart' as app;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mockito/mockito.dart';
 import 'package:starter_architecture_flutter_firebase/main.dart';
 import 'package:flutter/material.dart';
@@ -9,10 +10,14 @@ import 'fake_auth_service.dart';
 
 class MockDatabase extends Mock implements FirestoreDatabase {}
 
-void main() {
+Future<void> main() async {
   // This line enables the extension.
   enableFlutterDriverExtension();
 
+  // TODO: Somehow Firebase.initializeApp() is required when running this driver test
+  // Need to figure out what code path triggers this as both FirebaseAuth and Firestore are mocked
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   // Call the `main()` function of the app, or call `runApp` with
   // any widget you are interested in testing.
   runApp(MyApp(
