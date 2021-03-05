5a183ba11fc1c1108e9064f1e5a598f5d2c70c38
Andrea Bizzotto <bizz84@gmail.com>
2021-03-05 14:38:15 +0000

    Comment out flutter_driver tests (will be replaced by integration_test)

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,34 +1,34 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mockito/mockito.dart';
-import 'package:starter_architecture_flutter_firebase/app/top_level_providers.dart';
-import 'package:starter_architecture_flutter_firebase/main.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:starter_architecture_flutter_firebase/services/firestore_database.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:mockito/mockito.dart';
+// import 'package:starter_architecture_flutter_firebase/app/top_level_providers.dart';
+// import 'package:starter_architecture_flutter_firebase/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_driver/driver_extension.dart';
+// import 'package:starter_architecture_flutter_firebase/services/firestore_database.dart';
 
-import 'fake_auth_service.dart';
+// import 'fake_auth_service.dart';
 
-class MockDatabase extends Mock implements FirestoreDatabase {}
+// class MockDatabase extends Mock implements FirestoreDatabase {}
 
-// Run with:
-// flutter drive --target=test_driver/app.dart
-Future<void> main() async {
-  // This line enables the extension.
-  enableFlutterDriverExtension();
+// // Run with:
+// // flutter drive --target=test_driver/app.dart
+// Future<void> main() async {
+//   // This line enables the extension.
+//   enableFlutterDriverExtension();
 
-  // TODO: Somehow Firebase.initializeApp() is required when running this driver test
-  // Need to figure out what code path triggers this as both FirebaseAuth and Firestore are mocked
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  // Call the `main()` function of the app, or call `runApp` with
-  // any widget you are interested in testing.
-  runApp(ProviderScope(
-    overrides: [
-      firebaseAuthProvider
-          .overrideWithProvider(Provider((ref) => FakeAuthService())),
-      databaseProvider.overrideWithProvider(Provider((ref) => MockDatabase())),
-    ],
-    child: MyApp(),
-  ));
-}
+//   // TODO: Somehow Firebase.initializeApp() is required when running this driver test
+//   // Need to figure out what code path triggers this as both FirebaseAuth and Firestore are mocked
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   // Call the `main()` function of the app, or call `runApp` with
+//   // any widget you are interested in testing.
+//   runApp(ProviderScope(
+//     overrides: [
+//       firebaseAuthProvider
+//           .overrideWithProvider(Provider((ref) => FakeAuthService())),
+//       databaseProvider.overrideWithProvider(Provider((ref) => MockDatabase())),
+//     ],
+//     child: MyApp(),
+//   ));
+// }
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,71 +1,71 @@
-// Some introductory articles about integration tests:
-// http://cogitas.net/write-integration-test-flutter/
-// https://medium.com/flutter-community/testing-flutter-ui-with-flutter-driver-c1583681e337
-// https://stackoverflow.com/questions/52462646/how-to-solve-not-found-dartui-error-while-running-integration-tests-on-flutt
-//
-// Issues with opening the drawer with flutter driver
-// https://github.com/flutter/flutter/issues/9002
-//
-// Rules:
-// - Don't import any flutter code (e.g. material.dart)
-// - Don't import flutter_test.dart
+// // Some introductory articles about integration tests:
+// // http://cogitas.net/write-integration-test-flutter/
+// // https://medium.com/flutter-community/testing-flutter-ui-with-flutter-driver-c1583681e337
+// // https://stackoverflow.com/questions/52462646/how-to-solve-not-found-dartui-error-while-running-integration-tests-on-flutt
+// //
+// // Issues with opening the drawer with flutter driver
+// // https://github.com/flutter/flutter/issues/9002
+// //
+// // Rules:
+// // - Don't import any flutter code (e.g. material.dart)
+// // - Don't import flutter_test.dart
 
-import 'package:starter_architecture_flutter_firebase/constants/keys.dart';
-// Imports the Flutter Driver API.
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+// import 'package:starter_architecture_flutter_firebase/constants/keys.dart';
+// // Imports the Flutter Driver API.
+// import 'package:flutter_driver/flutter_driver.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  FlutterDriver driver;
-  Future<void> delay([int milliseconds = 250]) async {
-    await Future<void>.delayed(Duration(milliseconds: milliseconds));
-  }
+// void main() {
+//   FlutterDriver driver;
+//   Future<void> delay([int milliseconds = 250]) async {
+//     await Future<void>.delayed(Duration(milliseconds: milliseconds));
+//   }
 
-  // Connect to the Flutter driver before running any tests.
-  setUpAll(() async {
-    driver = await FlutterDriver.connect();
-  });
+//   // Connect to the Flutter driver before running any tests.
+//   setUpAll(() async {
+//     driver = await FlutterDriver.connect();
+//   });
 
-  // Close the connection to the driver after the tests have completed.
-  tearDownAll(() async {
-    if (driver != null) {
-      await driver.close();
-    }
-  });
+//   // Close the connection to the driver after the tests have completed.
+//   tearDownAll(() async {
+//     if (driver != null) {
+//       await driver.close();
+//     }
+//   });
 
-  test('check flutter driver health', () async {
-    final health = await driver.checkHealth();
-    expect(health.status, HealthStatus.ok);
-  });
+//   test('check flutter driver health', () async {
+//     final health = await driver.checkHealth();
+//     expect(health.status, HealthStatus.ok);
+//   });
 
-  test('sign in anonymously, sign out', () async {
-    // find and tap anonymous sign in button
-    final anonymousSignInButton = find.byValueKey(Keys.anonymous);
-    // Check to fail early if the auth state is authenticated
-    await driver.waitFor(anonymousSignInButton);
-    await delay(1000); // for video capture
-    await driver.tap(anonymousSignInButton);
+//   test('sign in anonymously, sign out', () async {
+//     // find and tap anonymous sign in button
+//     final anonymousSignInButton = find.byValueKey(Keys.anonymous);
+//     // Check to fail early if the auth state is authenticated
+//     await driver.waitFor(anonymousSignInButton);
+//     await delay(1000); // for video capture
+//     await driver.tap(anonymousSignInButton);
 
-    // Find tab bar and tap on account tab
-    // TODO This does not work. See: https://stackoverflow.com/questions/55460993/flutter-driver-test-bottomnavigationbaritem
-    final tabBar = find.byValueKey(Keys.tabBar);
-    await driver.waitFor(tabBar);
-    await delay(1000); // for video capture
-    await driver.tap(find.byValueKey(Keys.accountTab));
+//     // Find tab bar and tap on account tab
+//     // TODO This does not work. See: https://stackoverflow.com/questions/55460993/flutter-driver-test-bottomnavigationbaritem
+//     final tabBar = find.byValueKey(Keys.tabBar);
+//     await driver.waitFor(tabBar);
+//     await delay(1000); // for video capture
+//     await driver.tap(find.byValueKey(Keys.accountTab));
 
-    // find and tap logout button
-    final logoutButton = find.byValueKey(Keys.logout);
-    await driver.waitFor(logoutButton);
-    await delay(1000); // for video capture
-    await driver.tap(logoutButton);
+//     // find and tap logout button
+//     final logoutButton = find.byValueKey(Keys.logout);
+//     await driver.waitFor(logoutButton);
+//     await delay(1000); // for video capture
+//     await driver.tap(logoutButton);
 
-    // find and tap confirm logout button
-    final confirmLogoutButton = find.byValueKey(Keys.alertDefault);
-    await driver.waitFor(confirmLogoutButton);
-    await delay(1000); // for video capture
-    await driver.tap(confirmLogoutButton);
+//     // find and tap confirm logout button
+//     final confirmLogoutButton = find.byValueKey(Keys.alertDefault);
+//     await driver.waitFor(confirmLogoutButton);
+//     await delay(1000); // for video capture
+//     await driver.tap(confirmLogoutButton);
 
-    // try to find anonymous sign in button again
-    await driver.waitFor(anonymousSignInButton);
-  });
-}
+//     // try to find anonymous sign in button again
+//     await driver.waitFor(anonymousSignInButton);
+//   });
+// }
