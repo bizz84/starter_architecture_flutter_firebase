cbf3e687020879b5d98cb303626ba2a8ebc56bdf
bhryck <randall2000n@gmail.com>
2021-04-06 23:09:14 +0800

    hahahaha

--- lib/app/home/search/search_page.dart
+++ lib/app/home/search/search_page.dart
@@ -1,61 +1,150 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:starter_architecture_flutter_firebase/app/home/job_entries/edit_job_entries.dart';
-import 'package:starter_architecture_flutter_firebase/app/home/sells/add_sells_page.dart';
-import 'package:starter_architecture_flutter_firebase/app/home/sells/job_list_tile.dart';
-import 'package:starter_architecture_flutter_firebase/app/home/sells/list_items_builder.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/models/job.dart';
 import 'package:alert_dialogs/alert_dialogs.dart';
 import 'package:starter_architecture_flutter_firebase/app/top_level_providers.dart';
-import 'package:starter_architecture_flutter_firebase/constants/strings.dart';
-import 'package:pedantic/pedantic.dart';
+import 'package:starter_architecture_flutter_firebase/routing/app_router.dart';
 import 'package:starter_architecture_flutter_firebase/services/firestore_database.dart';
+import 'package:pedantic/pedantic.dart';
 
-final jobsStreamProvider = StreamProvider.autoDispose<List<Job>>((ref) {
-  final database = ref.watch(databaseProvider);
-  return database.jobsStream();
-});
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key, this.job}) : super(key: key);
+  final Job? job;
 
-// watch database
-class SearchPage extends ConsumerWidget {
-  Future<void> _delete(BuildContext context, Job job) async {
-    try {
-      final database = context.read<FirestoreDatabase>(databaseProvider);
-      await database.deleteJob(job);
-    } catch (e) {
-      unawaited(showExceptionAlertDialog(
-        context: context,
-        title: 'Operation failed',
-        exception: e,
-      ));
+  static Future<void> show(BuildContext context, {Job? job}) async {
+    await Navigator.of(context, rootNavigator: true).pushNamed(
+      AppRoutes.editJobPage,
+      arguments: job,
+    );
+  }
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _name;
+  String? _category;
+  @override
+  void initState() {
+    super.initState();
+    if (widget.job != null) {
+      _name = widget.job?.name;
+      _category = widget.job?.category;
+    }
+  }
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState!;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  Future<void> _search() async {
+    if (_validateAndSaveForm()) {
+      try {
+        final database = context.read<FirestoreDatabase>(databaseProvider);
+        final jobs = await database.jobsStream().first;
+      } catch (e) {
+        unawaited(showExceptionAlertDialog(
+          context: context,
+          title: 'Operation failed',
+          exception: e,
+        ));
+      }
     }
   }
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(Strings.search)
+        elevation: 2.0,
+        title: Text(widget.job == null ? 'New Product' : 'Edit Product'),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text(
+              'Save',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            onPressed: () => _search(),
+          ),
+        ],
       ),
-      body: _buildContents(context, watch),
+      body: _buildContents(),
+      backgroundColor: Colors.grey[200],
     );
   }
 
-  Widget _buildContents(BuildContext context, ScopedReader watch) {
-    final jobsAsyncValue = watch(jobsStreamProvider);
-    return ListItemsBuilder<Job>(
-      data: jobsAsyncValue,
-      itemBuilder: (context, job) => Dismissible(
-        key: Key('job-${job.id}'),
-        background: Container(color: Colors.red),
-        direction: DismissDirection.endToStart,
-        onDismissed: (direction) => _delete(context, job),
-        child: JobListTile(
-          job: job,
-          onTap: () => JobEntriesPage.show(context, job),
+  Widget _buildContents() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: _buildForm(),
+          ),
         ),
       ),
     );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildFormChildren(),
+      ),
+    );
+  }
+
+  var _categories = ['phones', 'laptop', 'others'];
+  String _currentSelectedValue = 'phones';
+  List<Widget> _buildFormChildren() {
+    return [
+      TextFormField(
+        decoration: const InputDecoration(labelText: 'Product name'),
+        keyboardAppearance: Brightness.light,
+        initialValue: _name,
+        validator: (value) =>
+            (value ?? '').isNotEmpty ? null : 'Name can\'t be empty',
+        onSaved: (value) => _name = value,
+      ),
+      FormField<String>(
+        builder: (FormFieldState<String> state) {
+          return InputDecorator(
+            decoration: InputDecoration(labelText: 'Categories'),
+            isEmpty: _currentSelectedValue == '',
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                value: _currentSelectedValue,
+                isDense: true,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _currentSelectedValue = newValue!;
+                    state.didChange(newValue);
+                  });
+                },
+                items: _categories.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+          );
+        },
+        onSaved: (value) => _category = value,
+      )
+    ];
   }
 }
