a45bcb223aaf918a1d3e596e1bb7680bd543eefd
“adiletD” <daniiarov.adilet@gmail.com>
2021-04-11 01:52:10 +0800

    updated details page. now only for history

--- lib/app/home/history/history_details.dart
+++ lib/app/home/history/history_details.dart
@@ -6,11 +6,10 @@ import 'package:starter_architecture_flutter_firebase/app/top_level_providers.da
 import 'package:flutter/material.dart';
 
 final itemStreamProvider =
-StreamProvider.autoDispose.family<Item, String>((ref, itemId) {
+    StreamProvider.autoDispose.family<Item, String>((ref, itemId) {
   final database = ref.watch(databaseProvider);
   return database.itemStream(itemId: itemId);
 });
-
 
 class ItemEntriesAppBarTitle extends ConsumerWidget {
   const ItemEntriesAppBarTitle({required this.item});
@@ -38,66 +37,115 @@ class SellProductPage extends StatelessWidget {
       arguments: item,
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final firebaseAuth = context.read(firebaseAuthProvider);
     final user = firebaseAuth.currentUser!;
     return Scaffold(
       appBar: AppBar(
-          title: ItemEntriesAppBarTitle(item: item)
+        title: ItemEntriesAppBarTitle(item: item),
+        centerTitle: true,
       ),
       body: _buildItemDetails(item, context),
-
     );
   }
 
   Widget _buildItemDetails(Item item, BuildContext context) {
     // align for sold item
-    Align align; 
-    if (item.bought){ 
-          align =  Align(
-          alignment: Alignment.bottomCenter,
-          child: RaisedButton(
-              onPressed: null,
-              child: const Text('Sold', style: TextStyle(fontSize: 20)),
-              color: Colors.grey,
-              textColor: Colors.white,
-              elevation: 5
-          ),
-        );
-    }
-    else{
-          align =  Align(
-          alignment: Alignment.bottomCenter,
-          child: RaisedButton(
-              onPressed: () => null,
-              child: const Text('on listing', style: TextStyle(fontSize: 20)),
-              color: Colors.grey,
-              textColor: Colors.white,
-              elevation: 5
-          ),
-        );
+    Align align;
+    String buttonText;
+    String price = item.price.toString();
+    if (item.bought) {
+      buttonText = 'Sold';
+      // align = Align(
+      //   alignment: Alignment.bottomCenter,
+      //   child: RaisedButton(
+      //       onPressed: null,
+      //       child: const Text('Sold', style: TextStyle(fontSize: 20)),
+      //       color: Colors.grey,
+      //       textColor: Colors.white,
+      //       elevation: 5),
+      // );
+    } else {
+      buttonText = 'On Listing';
+      // align = Align(
+      //   alignment: Alignment.bottomRight,
+      //   child: RaisedButton(
+      //       onPressed: () => null,
+      //       child: const Text('on listing', style: TextStyle(fontSize: 20)),
+      //       color: Colors.grey,
+      //       textColor: Colors.white,
+      //       elevation: 5),
+      // );
     }
     return Column(
       children: [
-        Text(
-          'Name: ${item.name}',
-          style: const TextStyle(color: Colors.black),
+        // Image.asset("assets/mac.jpeg"),
+        Container(
+          width: 300,
+          height: 300,
+          child: Image.network(
+              'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-202005?wid=892&hei=820&&qlt=80&.v=1587460552755'),
         ),
-        Text(
-          item.price.toString(),
-          style: const TextStyle(color: Colors.black),
-        ),
-        Text(
-          item.description,
-          style: const TextStyle(color: Colors.black),
-        ),
-        Text(
-          item.category,
-          style: const TextStyle(color: Colors.black),
-        ),
-        align, 
+        Container(
+            child: Text(
+              item.description,
+              style: const TextStyle(height: 1.5, color: Colors.black),
+              textAlign: TextAlign.justify,
+            ),
+            padding: EdgeInsets.all(16)),
+        Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Price: \$$price',
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 28),
+                      ),
+                      ButtonTheme(
+                        minWidth: 200.0,
+                        height: 50.0,
+                        child: RaisedButton(
+                            onPressed: () => null,
+                            child: Text(buttonText,
+                                style: TextStyle(fontSize: 20)),
+                            color: Colors.grey,
+                            textColor: Colors.white,
+                            elevation: 5),
+                      ),
+
+                      // RaisedButton(
+
+                      // FlatButton(
+                      //     onPressed: null,
+                      //     child: Text(
+                      //       "Add to Bag +",
+                      //       style:
+                      //           TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                      //     )),
+                    ],
+                  )),
+            ]),
+
+        // Text(
+        //   'Name: ${item.name}',
+        //   style: const TextStyle(color: Colors.black),
+        // ),
+        // Text(
+        //   item.category,
+        //   style: const TextStyle(color: Colors.black),
+        // ),
+        // align,
       ],
     );
   }
@@ -105,5 +153,3 @@ class SellProductPage extends StatelessWidget {
   //
   // }
 }
-
-
