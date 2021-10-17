22ddb139c8cef692f370307623bfdaff54efe597
Andrea Bizzotto <bizz84@gmail.com>
2021-10-17 08:39:40 +0100

    Update to Riverpod 1.0.0-dev.10

--- lib/app/home/job_entries/job_entries_page.dart
+++ lib/app/home/job_entries/job_entries_page.dart
@@ -1,19 +1,19 @@
 import 'dart:async';
 
+import 'package:alert_dialogs/alert_dialogs.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pedantic/pedantic.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/job_entries/entry_list_item.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/job_entries/entry_page.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/jobs/edit_job_page.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/jobs/list_items_builder.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/models/entry.dart';
 import 'package:starter_architecture_flutter_firebase/app/home/models/job.dart';
-import 'package:alert_dialogs/alert_dialogs.dart';
 import 'package:starter_architecture_flutter_firebase/app/top_level_providers.dart';
 import 'package:starter_architecture_flutter_firebase/routing/cupertino_tab_view_router.dart';
-import 'package:pedantic/pedantic.dart';
 
 class JobEntriesPage extends StatelessWidget {
   const JobEntriesPage({required this.job});
@@ -70,7 +70,7 @@ class JobEntriesAppBarTitle extends ConsumerWidget {
     final jobAsyncValue = ref.watch(jobStreamProvider(job.id));
     return jobAsyncValue.when(
       data: (job) => Text(job.name),
-      loading: (job) => Container(),
+      loading: (_) => Container(),
       error: (_, __, ___) => Container(),
     );
   }
