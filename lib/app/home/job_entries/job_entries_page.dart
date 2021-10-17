6f681f39e21e16acc760cc0280a3d287610dc7a4
gradycsjohnson <gradycsjohnson@gmail.com>
2021-10-17 03:36:19 -0400

    fix-build-errors (#85)

--- lib/app/home/job_entries/job_entries_page.dart
+++ lib/app/home/job_entries/job_entries_page.dart
@@ -70,8 +70,8 @@ class JobEntriesAppBarTitle extends ConsumerWidget {
     final jobAsyncValue = ref.watch(jobStreamProvider(job.id));
     return jobAsyncValue.when(
       data: (job) => Text(job.name),
-      loading: () => Container(),
-      error: (_, __) => Container(),
+      loading: (job) => Container(),
+      error: (_, __, ___) => Container(),
     );
   }
 }
