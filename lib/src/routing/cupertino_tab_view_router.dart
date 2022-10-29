4318fe313d209ae9b24704c62c55329ac82cc6e0
Andrea Bizzotto <codewithandrea@gmail.com>
2022-10-29 20:35:42 +0100

    Fix import

--- lib/src/routing/cupertino_tab_view_router.dart
+++ lib/src/routing/cupertino_tab_view_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:starter_architecture_flutter_firebase/src/features/home/models/job.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/job_entries/job_entries_page.dart';
-import 'package:starter_architecture_flutter_firebase/src/home/models/job.dart';
 
 class CupertinoTabViewRoutes {
   static const jobEntriesPage = '/job-entries-page';
