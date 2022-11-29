9fc421d59ad0a72db8052271e9fcf3cc45b8d9bb
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-29 14:59:27 +0000

    Fix to prevent too many redirects on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:starter_architecture_flutter_firebase/firebase_options.dart';
 import 'package:starter_architecture_flutter_firebase/src/app.dart';
+import 'package:starter_architecture_flutter_firebase/src/features/authentication/data/firebase_auth_repository.dart';
 import 'package:starter_architecture_flutter_firebase/src/localization/string_hardcoded.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/onboarding/data/onboarding_repository.dart';
 
@@ -19,12 +20,19 @@ Future<void> main() async {
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
   // * Entry point of the app
-  runApp(ProviderScope(
+
+  final container = ProviderContainer(
     overrides: [
       onboardingRepositoryProvider.overrideWithValue(
         OnboardingRepository(sharedPreferences),
       ),
     ],
+  );
+  // await until auth state is determined
+  // this will prevent unnecessary redirects inside GoRouter when the app starts
+  await container.read(authStateChangesProvider.future);
+  runApp(UncontrolledProviderScope(
+    container: container,
     child: MyApp(),
   ));
 }
