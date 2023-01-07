4d65a1b37fa62bdaa7ac0cc6e592a2113163e207
Andrea Bizzotto <codewithandrea@gmail.com>
2023-01-07 16:20:52 +0000

    Removed logger package

--- lib/src/utils/logger_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
-
-final loggerProvider = Provider<Logger>((ref) => Logger(
-      printer: PrettyPrinter(
-        methodCount: 1,
-        printEmojis: false,
-      ),
-    ));
