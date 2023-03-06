7bacc052e72c924f21a404d2a889713fb89845d4
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-06 09:15:33 +0000

    Fix failing test by expecting a TypeError

--- test/src/features/jobs/domain/job_test.dart
+++ test/src/features/jobs/domain/job_test.dart
@@ -12,11 +12,14 @@ void main() {
     });
 
     test('missing name', () {
+      // * If the 'name' is missing, this error will be emitted:
+      // * _CastError:<type 'Null' is not a subtype of type 'String' in type cast>
+      // * We can detect it by expecting that the test throws a TypeError
       expect(
           () => Job.fromMap(const {
                 'ratePerHour': 10,
               }, 'abc'),
-          throwsA(isInstanceOf<StateError>()));
+          throwsA(isInstanceOf<TypeError>()));
     });
   });
 
