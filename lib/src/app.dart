459ba5ec2d0c34b734bb913ebda090e9b488ab10
Andrea Bizzotto <codewithandrea@gmail.com>
2024-01-13 15:36:07 +0000

    Update primary theme color for AppBar, FloatingActionButton

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,15 +5,19 @@ import 'package:starter_architecture_flutter_firebase/src/routing/app_router.dar
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
+  static const primaryColor = Colors.indigo;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final goRouter = ref.watch(goRouterProvider);
     return MaterialApp.router(
       routerConfig: goRouter,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        colorSchemeSeed: primaryColor,
         unselectedWidgetColor: Colors.grey,
         appBarTheme: const AppBarTheme(
+          backgroundColor: primaryColor,
+          foregroundColor: Colors.white,
           elevation: 2.0,
           centerTitle: true,
         ),
@@ -22,9 +26,12 @@ class MyApp extends ConsumerWidget {
         // https://github.com/firebase/flutterfire/blob/master/packages/firebase_ui_auth/doc/theming.md
         outlinedButtonTheme: OutlinedButtonThemeData(
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo),
+            backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
             foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
           ),
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primaryColor,
         ),
       ),
       debugShowCheckedModeBanner: false,
