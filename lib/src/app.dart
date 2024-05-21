1ac9ab0e7c7d7831c89ea7a58c94804b97c3075e
Andrea Bizzotto <codewithandrea@gmail.com>
2024-05-21 09:03:02 +0100

    Updated packages

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -23,17 +23,16 @@ class MyApp extends ConsumerWidget {
         ),
         scaffoldBackgroundColor: Colors.grey[200],
         dividerColor: Colors.grey[400],
-        // https://github.com/firebase/flutterfire/blob/master/packages/firebase_ui_auth/doc/theming.md
         elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
-            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: primaryColor,
+            foregroundColor: Colors.white,
           ),
         ),
         outlinedButtonTheme: OutlinedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
-            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          style: OutlinedButton.styleFrom(
+            backgroundColor: primaryColor,
+            foregroundColor: Colors.white,
           ),
         ),
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
